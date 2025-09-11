617ca524d98f3222140c00e8fe8146f3fd00973c
Sam Foreman <saforem2@gmail.com>
2025-09-11 18:56:29 -0500

    feat: Use `ezpz` for logging

--- torchtitan/tools/logging.py
+++ torchtitan/tools/logging.py
@@ -4,18 +4,18 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import ezpz
 import logging
 import os
-import sys
 
 
-logger = logging.getLogger()
+logger = ezpz.get_logger(__name__)
 
 
 def init_logger():
-    logger.setLevel(logging.INFO)
-    logger.handlers.clear()
-    ch = logging.StreamHandler(sys.stdout)
+    level = logging.INFO if ezpz.get_rank() == 0 else logging.CRITICAL
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
     formatter = logging.Formatter(
         "[titan] %(asctime)s - %(name)s - %(levelname)s - %(message)s"
