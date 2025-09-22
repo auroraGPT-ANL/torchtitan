bf6f988024ab88dbe6e46de048f88065e1f71957
Sam Foreman <saforem2@gmail.com>
2025-09-22 11:16:43 -0500

    chore: Add `ezpz` to `tools/logging.py`

--- torchtitan/tools/logging.py
+++ torchtitan/tools/logging.py
@@ -4,9 +4,10 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import ezpz
 import logging
 import os
+
+import ezpz
 
 
 logger = ezpz.get_logger(__name__)
